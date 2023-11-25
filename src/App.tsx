cc41f83fd93a00d620ebeb65cff52e90baa9d31c
MuhdHanish <muhammedhanish11@gmail.com>
2023-11-25 19:53:22 +0530

    lesson 1 - typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
+import { MUITypography } from './components/MUITypography'
 
 function App() {
   return (
     <>
       <div>
-      
-        
+      <MUITypography/>
       </div>
     </>
   )
---
+++ src/components/MUITypography.tsx
@@ -0,0 +1,24 @@
+import { Typography } from "@mui/material"
+
+export const MUITypography = () => {
+  return (
+    <div>
+      <Typography variant="h1" gutterBottom >H1 Heading</Typography>
+      <Typography variant="h2">H2 Heading</Typography>
+      <Typography variant="h3">H3 Heading</Typography>
+      <Typography variant="h4">H4 Heading</Typography>
+      <Typography variant="h5">H5 Heading</Typography>
+      <Typography variant="h6">H6 Heading</Typography>
+      
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
+      
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, fugiat voluptate tempora ipsam, voluptates maxime nam rerum hic quos dolor quibusdam ratione sequi voluptatum obcaecati iure similique sit nisi. In.
+      </Typography>
+      <Typography variant="body2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos dolore labore accusantium ratione nisi nostrum obcaecati, quasi eligendi ad. Quis laboriosam minus beatae? Veritatis harum beatae sapiente ipsum, doloremque non?
+      </Typography>
+    </div>
+  );
+}
