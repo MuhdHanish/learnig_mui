46d128ae281e8eb694cda94610c53459522d5b91
MuhdHanish <muhammedhanish11@gmail.com>
2023-12-24 14:46:05 +0530

    lesson 19 - Menu

--- src/components/MUINavbar.tsx
+++ src/components/MUINavbar.tsx
@@ -1,7 +1,26 @@
-import { AppBar, Button, IconButton, Stack, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import SailingIcon from "@mui/icons-material/Sailing";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import React, { useState } from "react";
 
 export const MUINavbar = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar position="static">
       <Toolbar>
@@ -11,10 +30,33 @@ export const MUINavbar = () => {
         <Typography variant="h6" component={"div"} sx={{ flexGrow: 1 }}>
           Midjourney
         </Typography>
-        <Stack direction={'row'} spacing={2}>
-            <Button color="inherit">Features</Button>
-            <Button color="inherit">Home</Button>
+        <Stack direction={"row"} spacing={2}>
+          <Button color="inherit">Features</Button>
+          <Button
+            color="inherit"
+            id="resources-button"
+            onClick={handleClick}
+            aria-controls={open ? "true" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            endIcon={<KeyboardArrowDownIcon/>}
+          >
+            Resources
+          </Button>
+          <Button color="inherit">Home</Button>
         </Stack>
+        <Menu
+          id="resources-menu"
+          anchorEl={anchorEl}
+          open={open}
+          MenuListProps={{ "aria-labelledby": "resources-button" }}
+          onClose={handleClose}
+          anchorOrigin={{vertical:"bottom", horizontal:"right"}}
+          transformOrigin={{vertical:"top", horizontal:"right"}}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
